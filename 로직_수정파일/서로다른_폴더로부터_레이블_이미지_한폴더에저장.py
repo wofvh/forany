c7687e47e1cf352795292d7c1423e0eb14188d6a
teddy <79746587+wofvh@users.noreply.github.com>
2023-08-17 09:33:14 +0900

    Create 서로다른_폴더로부터_레이블_이미지_한폴더에저장.py

---
+++ 로직_수정파일/서로다른_폴더로부터_레이블_이미지_한폴더에저장.py
@@ -0,0 +1,63 @@
+##############서로다른 폴더에서 불러오기###########################
+import os
+import shutil
+
+folder1_path = 'F:/coco128_last/val2017/images/'  
+folder2_path = 'F:/coco128_last/val2017/labels/' 
+destination_folder = 'F:/coco128_last/last/' 
+
+def merge_matching_files(folder1, folder2, destination):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+    
+    for root1, _, filenames1 in os.walk(folder1):
+        for root2, _, filenames2 in os.walk(folder2):
+            for filename1 in filenames1:
+                base_name1, extension1 = os.path.splitext(filename1)
+                if extension1 == '.txt':
+                    jpg_filename1 = base_name1 + '.jpg'
+                    txt_path1 = os.path.join(root1, filename1)
+                    jpg_path1 = os.path.join(root1, jpg_filename1)
+                    for filename2 in filenames2:
+                        base_name2, extension2 = os.path.splitext(filename2)
+                        if extension2 == '.txt' and base_name2 == base_name1:
+                            txt_path2 = os.path.join(root2, filename2)
+                            jpg_path2 = os.path.join(root2, base_name2 + '.jpg')
+                            if os.path.exists(txt_path2) and os.path.exists(jpg_path2):
+                                destination_txt = os.path.join(destination, filename1)
+                                destination_jpg = os.path.join(destination, jpg_filename1)
+                                shutil.copy(txt_path1, destination_txt)
+                                shutil.copy(jpg_path1, destination_jpg)
+                                break
+
+merge_matching_files(folder1_path, folder2_path, destination_folder)
+print("파일 합치기 완료")
+
+#####################폴더 하나에서 불러오기##############
+import os
+import shutil
+
+source_folder = 'F:/coco128_last/val2017/images/'    # 수정 필요
+destination_folder = 'F:/coco128_last/last/'  # 수정 필요
+
+def merge_matching_files(source, destination):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+    
+    copied_files = set()  # 이미 복사한 파일들을 추적하기 위한 집합
+    
+    for filename in os.listdir(source):
+        base_name, extension = os.path.splitext(filename)
+        if extension == '.txt':
+            jpg_filename = base_name + '.jpg'
+            txt_path = os.path.join(source, filename)
+            jpg_path = os.path.join(source, jpg_filename)
+            if os.path.exists(jpg_path) and filename not in copied_files:
+                destination_txt = os.path.join(destination, filename)
+                destination_jpg = os.path.join(destination, jpg_filename)
+                shutil.copy(txt_path, destination_txt)
+                shutil.copy(jpg_path, destination_jpg)
+                copied_files.add(filename)
+
+merge_matching_files(source_folder, destination_folder)
+print("파일 합치기 완료")
