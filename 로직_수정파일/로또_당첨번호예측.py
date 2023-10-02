015472fbe09f85e859212d9297f6d0cd56c4f96e
teddy <79746587+wofvh@users.noreply.github.com>
2023-10-02 19:25:01 +0900

    Update 로또_당첨번호예측.py

--- 로직_수정파일/로또_당첨번호예측.py
+++ 로직_수정파일/로또_당첨번호예측.py
@@ -5,70 +5,70 @@ from bs4 import BeautifulSoup
 main_url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin" # 마지막 회차를 얻기 위한 주소
 basic_url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=" # 임의의 회차를 얻기 위한 주소
 
-# # 마지막 회차 정보를 가져옴
-# def GetLast(): 
-#     resp = requests.get(main_url)
-#     soup = BeautifulSoup(resp.text, "lxml")
-#     result = str(soup.find("meta", {"id" : "desc", "name" : "description"})['content'])
-#     s_idx = result.find(" ")
+# 마지막 회차 정보를 가져옴
+def GetLast(): 
+    resp = requests.get(main_url)
+    soup = BeautifulSoup(resp.text, "lxml")
+    result = str(soup.find("meta", {"id" : "desc", "name" : "description"})['content'])
+    s_idx = result.find(" ")
     
-#     e_idx = result.find("회")
-#     return int(result[s_idx + 1 : e_idx])
+    e_idx = result.find("회")
+    return int(result[s_idx + 1 : e_idx])
 
-# # 지정된 파일에 지정된 범위의 회차 정보를 기록함
-# def Crawler(s_count, e_count, fp):
-#     for i in range(s_count , e_count + 1):
-#         crawler_url = basic_url + str(i)
-#         resp = requests.get(crawler_url)
-#         soup = BeautifulSoup(resp.text, "html.parser")
+# 지정된 파일에 지정된 범위의 회차 정보를 기록함
+def Crawler(s_count, e_count, fp):
+    for i in range(s_count , e_count + 1):
+        crawler_url = basic_url + str(i)
+        resp = requests.get(crawler_url)
+        soup = BeautifulSoup(resp.text, "html.parser")
 
-#         text = soup.text
+        text = soup.text
 
-#         s_idx = text.find(" 당첨결과")
-#         s_idx = text.find("당첨번호", s_idx) + 4
-#         e_idx = text.find("보너스", s_idx)
-#         numbers = text[s_idx:e_idx].strip().split()
+        s_idx = text.find(" 당첨결과")
+        s_idx = text.find("당첨번호", s_idx) + 4
+        e_idx = text.find("보너스", s_idx)
+        numbers = text[s_idx:e_idx].strip().split()
 
-#         s_idx = e_idx + 3
-#         e_idx = s_idx + 3
-#         bonus = text[s_idx:e_idx].strip()
+        s_idx = e_idx + 3
+        e_idx = s_idx + 3
+        bonus = text[s_idx:e_idx].strip()
 
-#         s_idx = text.find("1등", e_idx) + 2
-#         e_idx = text.find("원", s_idx) + 1
-#         e_idx = text.find("원", e_idx)
-#         money1 = text[s_idx:e_idx].strip().replace(',','').split()[2]
+        s_idx = text.find("1등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money1 = text[s_idx:e_idx].strip().replace(',','').split()[2]
 
-#         s_idx = text.find("2등", e_idx) + 2
-#         e_idx = text.find("원", s_idx) + 1
-#         e_idx = text.find("원", e_idx)
-#         money2 = text[s_idx:e_idx].strip().replace(',','').split()[2]
+        s_idx = text.find("2등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money2 = text[s_idx:e_idx].strip().replace(',','').split()[2]
 
-#         s_idx = text.find("3등", e_idx) + 2
-#         e_idx = text.find("원", s_idx) + 1
-#         e_idx = text.find("원", e_idx)
-#         money3 = text[s_idx:e_idx].strip().replace(',','').split()[2]
+        s_idx = text.find("3등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money3 = text[s_idx:e_idx].strip().replace(',','').split()[2]
 
-#         s_idx = text.find("4등", e_idx) + 2
-#         e_idx = text.find("원", s_idx) + 1
-#         e_idx = text.find("원", e_idx)
-#         money4 = text[s_idx:e_idx].strip().replace(',','').split()[2]
+        s_idx = text.find("4등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money4 = text[s_idx:e_idx].strip().replace(',','').split()[2]
 
-#         s_idx = text.find("5등", e_idx) + 2
-#         e_idx = text.find("원", s_idx) + 1
-#         e_idx = text.find("원", e_idx)
-#         money5 = text[s_idx:e_idx].strip().replace(',','').split()[2]
+        s_idx = text.find("5등", e_idx) + 2
+        e_idx = text.find("원", s_idx) + 1
+        e_idx = text.find("원", e_idx)
+        money5 = text[s_idx:e_idx].strip().replace(',','').split()[2]
 
-#         line = str(i) + ',' + numbers[0] + ',' + numbers[1] + ',' + numbers[2] + ',' + numbers[3] + ',' + numbers[4] + ',' + numbers[5] + ',' + bonus + ',' + money1 + ',' + money2 + ',' + money3 + ',' + money4 + ',' + money5
-#         print(line)
-#         line += '\n'
-#         fp.write(line)
+        line = str(i) + ',' + numbers[0] + ',' + numbers[1] + ',' + numbers[2] + ',' + numbers[3] + ',' + numbers[4] + ',' + numbers[5] + ',' + bonus + ',' + money1 + ',' + money2 + ',' + money3 + ',' + money4 + ',' + money5
+        print(line)
+        line += '\n'
+        fp.write(line)
 
-# last = GetLast() # 마지막 회차를 가져옴
+last = GetLast() # 마지막 회차를 가져옴
 
-# fp = open('2020-1-25-keras_lstm_lotto_v895_data.csv', 'w')
-# Crawler(1, last, fp) # 처음부터 마지막 회차까지 저장
-# fp.close()
-# print(fp.read())
+fp = open('2020-1-25-keras_lstm_lotto_v895_data.csv', 'w')
+Crawler(1, last, fp) # 처음부터 마지막 회차까지 저장
+fp.close()
+print(fp.read())
 
 import numpy as np
 
@@ -121,16 +121,14 @@ val_idx = (700, 800)
 test_idx = (800, len(x_samples))
 
 print("train: {0}, val: {1}, test: {2}".format(train_idx, val_idx, test_idx))
-
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.python.keras.models import Sequential, Model
-from tensorflow.python.keras.layers import Dense, Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
 
 # 모델을 정의합니다.
-model = keras.Sequential([
-    keras.layers.LSTM(128, batch_input_shape=(1, 1, 45), return_sequences=False, stateful=True),
-    keras.layers.Dense(45, activation='sigmoid')
+model = Sequential([
+    LSTM(128, batch_input_shape=(1, 1, 45), return_sequences=False, stateful=True),
+    Dense(45, activation='sigmoid')
 ])
 
 # 모델을 컴파일합니다.
@@ -143,6 +141,7 @@ val_loss = []
 val_acc = []
 
 # 최대 100번 에포크까지 수행
+'''
 for epoch in range(100):
 
     model.reset_states() # 중요! 매 에포크마다 1회부터 다시 훈련하므로 상태 초기화 필요
@@ -160,8 +159,8 @@ for epoch in range(100):
         batch_train_loss.append(loss)
         batch_train_acc.append(acc)
 
-    train_loss.append(np.mean(batch_train_loss))
-    train_acc.append(np.mean(batch_train_acc))
+    train_loss.append(tf.reduce_mean(batch_train_loss))
+    train_acc.append(tf.reduce_mean(batch_train_acc))
 
     batch_val_loss = []
     batch_val_acc = []
@@ -176,9 +175,157 @@ for epoch in range(100):
         batch_val_loss.append(loss)
         batch_val_acc.append(acc)
 
-    val_loss.append(np.mean(batch_val_loss))
-    val_acc.append(np.mean(batch_val_acc))
+    val_loss.append(tf.reduce_mean(batch_val_loss))
+    val_acc.append(tf.reduce_mean(batch_val_acc))
 
-    print('epoch {0:4d} train acc {1:0.3f} loss {2:0.3f} val acc {3:0.3f} loss {4:0.3f}'.format(epoch, np.mean(batch_train_acc), np.mean(batch_train_loss), np.mean(batch_val_acc), np.mean(batch_val_loss)))
+    print('epoch {0:4d} train acc {1:0.3f} loss {2:0.3f} val acc {3:0.3f} loss {4:0.3f}'.format(epoch, tf.reduce_mean(batch_train_acc), tf.reduce_mean(batch_train_loss), tf.reduce_mean(batch_val_acc), tf.reduce_mean(batch_val_loss)))
 
     model.save('model_{0:04d}.h5'.format(epoch+1))
+'''
+
+mean_prize = [  np.mean(rows[87:, 8]),
+                np.mean(rows[87:, 9]),
+                np.mean(rows[87:, 10]),
+                np.mean(rows[87:, 11]),
+                np.mean(rows[87:, 12])]
+
+print(mean_prize)    
+
+# 등수와 상금을 반환함
+# 순위에 오르지 못한 경우에는 등수가 0으로 반환함
+def calc_reward(true_numbers, true_bonus, pred_numbers):
+
+    count = 0
+
+    for ps in pred_numbers:
+        if ps in true_numbers:
+            count += 1
+
+    if count == 6:
+        return 0, mean_prize[0]
+    elif count == 5 and true_bonus in pred_numbers:
+        return 1, mean_prize[1]
+    elif count == 5:
+        return 2, mean_prize[2]
+    elif count == 4:
+        return 3, mean_prize[3]
+    elif count == 3:
+        return 4, mean_prize[4]
+
+    return 5, 0
+
+def gen_numbers_from_probability(nums_prob):
+
+    ball_box = []
+
+    for n in range(45):
+        ball_count = int(nums_prob[n] * 100 + 1)
+        ball = np.full((ball_count), n+1) #1부터 시작
+        ball_box += list(ball)
+
+    selected_balls = []
+
+    while True:
+        
+        if len(selected_balls) == 6:
+            break
+        
+        ball_index = np.random.randint(len(ball_box), size=1)[0]
+        ball = ball_box[ball_index]
+
+        if ball not in selected_balls:
+            selected_balls.append(ball)
+
+    return selected_balls
+
+train_total_reward = []
+train_total_grade = np.zeros(6, dtype=int)
+
+val_total_reward = []
+val_total_grade = np.zeros(6, dtype=int)
+
+test_total_reward = []
+test_total_grade = np.zeros(6, dtype=int)
+
+model.reset_states()
+
+print('[No. ] 1st 2nd 3rd 4th 5th 6th Rewards')
+
+for i in range(len(x_samples)):
+    xs = x_samples[i].reshape(1, 1, 45)
+    ys_pred = model.predict_on_batch(xs) # 모델의 출력값을 얻음
+    
+    sum_reward = 0
+    sum_grade = np.zeros(6, dtype=int) # 6등까지 변수
+
+    for n in range(10): # 10판 수행
+        numbers = gen_numbers_from_probability(ys_pred[0])
+        
+        #i회차 입력 후 나온 출력을 i+1회차와 비교함
+        grade, reward = calc_reward(rows[i+1,1:7], rows[i+1,7], numbers) 
+
+        sum_reward += reward
+        sum_grade[grade] += 1
+
+        if i >= train_idx[0] and i < train_idx[1]:
+            train_total_grade[grade] += 1
+        elif i >= val_idx[0] and i < val_idx[1]:
+            val_total_grade[grade] += 1
+        elif i >= test_idx[0] and i < test_idx[1]:
+            val_total_grade[grade] += 1
+    
+    if i >= train_idx[0] and i < train_idx[1]:
+        train_total_reward.append(sum_reward)
+    elif i >= val_idx[0] and i < val_idx[1]:
+        val_total_reward.append(sum_reward)
+    elif i >= test_idx[0] and i < test_idx[1]:
+        test_total_reward.append(sum_reward)
+                        
+    print('[{0:4d}] {1:3d} {2:3d} {3:3d} {4:3d} {5:3d} {6:3d} {7:15,d}'.format(i+1, sum_grade[0], sum_grade[1], sum_grade[2], sum_grade[3], sum_grade[4], sum_grade[5], int(sum_reward)))
+
+print('Total') 
+print('==========')    
+print('Train {0:5d} {1:5d} {2:5d} {3:5d} {4:5d} {5:5d} {6:15,d}'.format(train_total_grade[0], train_total_grade[1], train_total_grade[2], train_total_grade[3], train_total_grade[4], train_total_grade[5], int(sum(train_total_reward))))
+print('Val   {0:5d} {1:5d} {2:5d} {3:5d} {4:5d} {5:5d} {6:15,d}'.format(val_total_grade[0], val_total_grade[1], val_total_grade[2], val_total_grade[3], val_total_grade[4], val_total_grade[5], int(sum(val_total_reward))))
+print('Test  {0:5d} {1:5d} {2:5d} {3:5d} {4:5d} {5:5d} {6:15,d}'.format(test_total_grade[0], test_total_grade[1], test_total_grade[2], test_total_grade[3], test_total_grade[4], test_total_grade[5], int(sum(test_total_reward))))
+print('==========')    
+
+print('receive numbers')
+
+xs = x_samples[-1].reshape(1, 1, 45)
+
+ys_pred = model.predict_on_batch(xs)
+
+list_numbers = []
+
+for n in range(5):
+    numbers = gen_numbers_from_probability(ys_pred[0])
+    print('{0} : {1}'.format(n, numbers))
+    list_numbers.append(numbers)  
+    
+
+    
+# from flask import Flask, render_template, request
+# import pickle
+# import numpy as np
+
+# # model = pickle.load(open('model_0100.h5', 'rb'))
+# model = tf.keras.models.load_model('./model_0100.h5')
+# app = Flask(__name__)
+
+# @app.route('/')
+# def main():
+#     return render_template('home.html')
+
+# @app.route('/predict', methods=['POST'])
+# def home():
+#     data1 = request.form['a']
+#     data2 = request.form['b']
+#     data3 = request.form['c']
+#     data4 = request.form['d']
+#     arr = np.array([[data1, data2, data3, data4]])
+#     pred = model.predict(arr)
+#     return render_template('after.html', data=pred)
+
+# if __name__ == "__main__":
+#     app.run(port=5001,debug=True)
