3523344f2cc6c99a5c717992e1a3803f9e674df2
teddy <79746587+wofvh@users.noreply.github.com>
2023-10-02 19:24:09 +0900

    Update and rename lotto예측_모델0906.py to lotto예측_모델_1002.py

---
+++ 로직_수정파일/lotto예측_모델_1002.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+main_url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin" # 마지막 회차를 얻기 위한 주소
+basic_url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=" # 임의의 회차를 얻기 위한 주소
+
+import numpy as np
+
+# 당첨번호를 원핫인코딩벡터(ohbin)으로 변환
+def numbers2ohbin(numbers):
+
+    ohbin = np.zeros(45) #45개의 빈 칸을 만듬
+
+    for i in range(6): #여섯개의 당첨번호에 대해서 반복함
+        ohbin[int(numbers[i])-1] = 1 #로또번호가 1부터 시작하지만 벡터의 인덱스 시작은 0부터 시작하므로 1을 뺌
+    
+    return ohbin
+
+# 원핫인코딩벡터(ohbin)를 번호로 변환
+def ohbin2numbers(ohbin):
+
+    numbers = []
+    
+    for i in range(len(ohbin)):
+        if ohbin[i] == 1.0: # 1.0으로 설정되어 있으면 해당 번호를 반환값에 추가한다.
+            numbers.append(i+1)
+    
+    return numbers
+
+print("1:" + str(numbers2ohbin([10,23,29,33,37,40])))
+print("2:" + str(numbers2ohbin([9,13,21,25,32,42])))
+
+rows = np.loadtxt("./lotto.csv", delimiter=",")
+row_count = len(rows)
+
+numbers = rows[:, 1:7]
+ohbins = list(map(numbers2ohbin, numbers))
+
+x_samples = ohbins[0:row_count-1]
+y_samples = ohbins[1:row_count]
+
+train_idx = (0, 700)
+val_idx = (700, 800)
+test_idx = (800, len(x_samples))
+
+print("train: {0}, val: {1}, test: {2}".format(train_idx, val_idx, test_idx))
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+# 모델을 정의합니다.
+model = Sequential([
+    LSTM(128, batch_input_shape=(1, 1, 45), return_sequences=False, stateful=True),
+    Dense(45, activation='sigmoid')
+])
+
+# 모델을 컴파일합니다.
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 매 에포크마다 훈련과 검증의 손실 및 정확도를 기록하기 위한 변수
+train_loss = []
+train_acc = []
+val_loss = []
+val_acc = []
+
+
+mean_prize = [  np.mean(rows[87:, 8]),
+                np.mean(rows[87:, 9]),
+                np.mean(rows[87:, 10]),
+                np.mean(rows[87:, 11]),
+                np.mean(rows[87:, 12])]   
+
+# 등수와 상금을 반환함
+# 순위에 오르지 못한 경우에는 등수가 0으로 반환함
+def calc_reward(true_numbers, true_bonus, pred_numbers):
+
+    count = 0
+
+    for ps in pred_numbers:
+        if ps in true_numbers:
+            count += 1
+
+    if count == 6:
+        return 0, mean_prize[0]
+    elif count == 5 and true_bonus in pred_numbers:
+        return 1, mean_prize[1]
+    elif count == 5:
+        return 2, mean_prize[2]
+    elif count == 4:
+        return 3, mean_prize[3]
+    elif count == 3:
+        return 4, mean_prize[4]
+
+    return 5, 0
+
+def gen_numbers_from_probability(nums_prob):
+
+    ball_box = []
+
+    for n in range(45):
+        ball_count = int(nums_prob[n] * 100 + 1)
+        ball = np.full((ball_count), n+1) #1부터 시작
+        ball_box += list(ball)
+
+    selected_balls = []
+
+    while True:
+        
+        if len(selected_balls) == 6:
+            break
+        
+        ball_index = np.random.randint(len(ball_box), size=1)[0]
+        ball = ball_box[ball_index]
+
+        if ball not in selected_balls:
+            selected_balls.append(ball)
+
+    return selected_balls
+
+train_total_reward = []
+train_total_grade = np.zeros(6, dtype=int)
+
+val_total_reward = []
+val_total_grade = np.zeros(6, dtype=int)
+
+test_total_reward = []
+test_total_grade = np.zeros(6, dtype=int)
+
+model.reset_states()
+
+print('receive numbers')
+
+xs = x_samples[-1].reshape(1, 1, 45)
+
+ys_pred = model.predict_on_batch(xs)
+
+list_numbers = []
+
+for n in range(7):
+    numbers = gen_numbers_from_probability(ys_pred[0])
+    numbers.sort() 
+    print('{0} : {1}'.format(n, numbers))
+    list_numbers.append(numbers)  
+    
