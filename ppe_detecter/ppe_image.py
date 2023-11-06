ff04c10b1bfc5beff574dc5663b301fca95c24af
teddy <79746587+wofvh@users.noreply.github.com>
2023-11-06 18:18:58 +0900

    Update ppe_image.py

--- ppe_detecter/ppe_image.py
+++ ppe_detecter/ppe_image.py
@@ -3,25 +3,18 @@ import cv2
 import numpy as np
 import datetime
 import time
-import os
 
+# names: [ "scaffolds","worker","boots","hardhat","safety_vest","hook","robot_dog","opened_hatch","closed_hatch",]
 def detect_ppe(image, model, timestamp):
     results = model(image)
-    # names: [ "scaffolds","worker","boots","hardhat","safety_vest","hook","robot_dog","opened_hatch","closed_hatch",]
-
-    # LABELS = ['scaffold', 'worker', 'hat', 'hook']  
     person = []
     hat = []
     hook = []
-    person_count = 0
-    hook_count = 0
-    o_hatch = 0
-    c_hatch = 0
     finalStatus = ""
     # missing_hooks = max(person_count - hook_count, 0)
     if np.shape(results.xyxy[0].cpu().numpy())[0] > 0:
         for (x0, y0, x1, y1, confi, clas) in results.xyxy[0].cpu().numpy():
-            if confi > 0.2:
+            if confi > 0.3:
                 # print(x0, y0, x1, y1, confi, clas)
                 box = [int(x0), int(y0), int(x1 - x0), int(y1 - y0)]
                 box2 = [int(x0), int(y0), int(x1), int(y1)]
@@ -39,27 +32,26 @@ def detect_ppe(image, model, timestamp):
                                 (0, 255, 0), 2)
                     hat.append(box2)  # Add the box coordinates to the person arrays
 
-                elif int(clas) == 7 :
-                    o_hatch = o_hatch + 1
-                    cv2.rectangle(image, box, (0, 0, 255), 2)
-                    cv2.putText(image, "opened_hatch {:.2f}".format(confi), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                                0.5,
-                                (0, 0, 255), 2)
+                # elif int(clas) == 7 :
+                #     cv2.rectangle(image, box, (0, 0, 255), 2)
+                #     cv2.putText(image, "opened_hatch {:.2f}".format(confi), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                #                 0.5,
+                #                 (0, 0, 255), 2)
                     
-                elif int(clas) == 8:
-                    c_hatch = c_hatch + 1
-                    cv2.rectangle(image, box, (0, 255, 0), 2)
-                    cv2.putText(image, "closed_hatch {:.2f}".format(confi), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                                0.5,
-                                (0, 255, 0), 2)
+                # elif int(clas) == 8:
+                #     cv2.rectangle(image, box, (0, 255, 0), 2)
+                #     cv2.putText(image, "closed_hatch {:.2f}".format(confi), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                #                 0.5,
+                #                 (0, 255, 0), 2)
                 elif int(clas) == 1 :
-                    person.append(box2)  
+                    person.append(box2)
+          
 
         class_worker_count = (results.xyxy[0].cpu().numpy()[:, -1] == 1).sum()
         class_hook_count = (results.xyxy[0].cpu().numpy()[:, -1] == 5).sum()
         class_helmet_count = (results.xyxy[0].cpu().numpy()[:, -1] == 3).sum()
-        class_opened_hatch = (results.xyxy[0].cpu().numpy()[:, -1] == 7).sum()
-        class_closed_hatch = (results.xyxy[0].cpu().numpy()[:, -1] == 8).sum()
+        # class_opened_hatch = (results.xyxy[0].cpu().numpy()[:, -1] == 7).sum()
+        # class_closed_hatch = (results.xyxy[0].cpu().numpy()[:, -1] == 8).sum()
 
         if class_hook_count >= class_worker_count:
             missing_hooks = 0
@@ -81,10 +73,10 @@ def detect_ppe(image, model, timestamp):
         else:
             finalStatus = "UnSafe"
 
-        if class_opened_hatch == 1:
-            finalStatus = "UnSafe"
-        elif class_closed_hatch == 1:
-            finalStatus = "Safe"
+        # if class_opened_hatch == 1:
+        #     finalStatus = "UnSafe"
+        # elif class_closed_hatch == 1:
+        #     finalStatus = "Safe"
 
         hatDetected = False
         for perBox in person:
@@ -114,10 +106,9 @@ def detect_ppe(image, model, timestamp):
         color_hooks = (0, 255, 0) if missing_hooks == 0 else (0, 0, 255)
         color_helmet = (0, 255, 0) if missing_helmet == 0 else (0, 0, 255)
 
-        cv2.putText(image, f"{finalStatus}", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color_status, 3)
-        cv2.putText(image, f"[Missing {missing_hooks} hooks]", (150, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.4, color_hooks, 2)
-        cv2.putText(image, f"[Missing {missing_helmet} helmet]", (150, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.4, color_helmet, 2)
-
+        cv2.putText(image, f"{finalStatus}", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color_status, 3)
+        cv2.putText(image, f"[Missing {missing_hooks} hooks]", (170, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color_hooks, 2)
+        cv2.putText(image, f"[Missing {missing_helmet} helmet]", (170, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color_helmet, 2)
 
     height, width, _ = image.shape
     formatted_datetime = timestamp.strftime("%Y-%m-%d %H:%M:%S")
@@ -125,25 +116,115 @@ def detect_ppe(image, model, timestamp):
 
     return image, results.xyxy[0].cpu().numpy(), timestamp, finalStatus
 
-image_folder_path = "D:/logic/Simplatform/images/"  # 입력 이미지가 있는 폴더 경로
-output_folder_path = "D:/logic/Simplatform/output/"  # 결과 이미지를 저장할 폴더 경로
-ppe_model = torch.hub.load('WongKinYiu/yolov7', 'custom', './PPE/best_410.pt', force_reload=False)
-# 폴더 내 모든 이미지 파일을 처리
-for filename in os.listdir(image_folder_path):
-    if filename.endswith(('.jpg', '.jpeg', '.png')):  # 이미지 파일 확장자 확인
-        image_path = os.path.join(image_folder_path, filename)
-        output_path = os.path.join(output_folder_path, f"result_{filename}")  # 결과 이미지를 저장할 경로
+recording = False
+video_path = "D:\logic\Simplatform/A_20230918_27.mp4"
+if __name__ == "__main__":
+    unsafe_threshold = 5  # Number of seconds to consider status as "UnSafe" # x = 5  # seconds
+    y = 1   # minutes
+    output_fps = 30
 
-        frame = cv2.imread(image_path)  # 이미지 읽기
+    ppe_model = torch.hub.load('WongKinYiu/yolov7', 'custom', './PPE/best_410.pt', force_reload=False)
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    desired_fps = 25  # Change it to the desired value
+    # cap.set(cv2.CAP_PROP_FPS, desired_fps)
 
+    prev_time = time.time()
+    video_count = 1
+    out = None  # Initialize the 'out' variable
+    video_writer = None # saving vdeio
+    unsafe_timer = 0.0
+    y_time_interval = y*60
+    percent_count = 0
+    mean_save = 20.0
+    unsafe_count = 0
+    safe_count = 0
+    save_video = 1
+    while True: 
+        ret, frame = cap.read()
+        if not ret:
+            break
         timestamp = datetime.datetime.now()
         status = ""
+        frame = cv2.resize(frame, (1920,1080))
+        
+        if video_writer is None:
+            save_video += 500
+            video_name = "./vdieo_save/video_{}.mp4".format(save_video)
+            height, width, _ = frame.shape
+            fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+            video_writer = cv2.VideoWriter(video_name, fourcc, output_fps, (width, height))
 
-        frame = cv2.resize(frame, (480, 480))  # 이미지 크기 조정
+        if out is None:
+            video_name = "./unsafe/video_{}.avi".format(video_count)
+            height, width, _ = frame.shape
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            out = cv2.VideoWriter(video_name, fourcc, output_fps, (width, height))
 
         ppe_result_image, bbox_lists, timestamp, status = detect_ppe(frame, ppe_model, timestamp)
-
-        cv2.imwrite(output_path, ppe_result_image)  # 결과 이미지 저장
+        video_writer.write(ppe_result_image)
 
         cv2.imshow("PPE Detection", ppe_result_image)
-        cv2.waitKey(0)
+        if status == "UnSafe":
+            unsafe_count += 1
+        if status == "" or status == "Safe":
+            status ="Safe"
+            safe_count += 1
+
+        current_time = time.time()
+        elapsed_time = current_time - prev_time
+        print("Time difference", elapsed_time)
+
+        if elapsed_time >= y_time_interval and recording :
+            if out is not None:
+                out.release()  # Release the previous video writer
+                out = None
+                video_count += 1
+                recording = False
+                prev_time = current_time
+                percent_count = 0
+                print("stop take vdieo")
+
+        if elapsed_time >= percent_count :  # Print percentages every 5 seconds
+            total_frames = unsafe_count + safe_count
+            unsafe_percent = (unsafe_count / total_frames) * 100.0
+            safe_percent = (safe_count / total_frames) * 100.0
+            print(f"UnSafe: {unsafe_percent:.2f}%  Safe: {safe_percent:.2f}%")
+            percent_count += 5
+            if status == "UnSafe" and recording == False:
+                if out is not None :
+                    out.write(ppe_result_image)
+                    if unsafe_percent >= mean_save:
+                        prev_time = current_time
+                        percent_count = 0
+                        recording = True
+                        print("unsafe")
+                        print("strat saving vdieo")
+                    else:
+                        if out is not None :
+                            out = None
+            unsafe_count = 0
+            safe_count = 0
+
+        if elapsed_time <= y_time_interval:# and elapsed_time >= unsafe_threshold:
+            if out is not None:
+                out.write(ppe_result_image)
+        else:
+            if out is not None :
+                out = None
+        if elapsed_time >= y_time_interval:
+            prev_time = current_time
+            percent_count = 0
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    if out is not None:
+        out.release()  # Release the video writer if it's still open
+        out = None
+    if video_writer is not None:
+        video_writer.release()
+        video_writer = None
+
+    cap.release()
+    cv2.destroyAllWindows()
